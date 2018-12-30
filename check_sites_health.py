d36cb41ca08e0fefdee47835876244cb4faee116
karadorum <contextstudio78@gmail.com>
2018-12-30 13:43:03 +0300

    PEP8 changes

--- check_sites_health.py
+++ check_sites_health.py
@@ -13,19 +13,20 @@ def console_args():
 
 def load_urls4check(path):
     with open(path, 'r', encoding='utf-8') as file:
-        urls_list =[url.replace('\n', '') for url in file]
+        urls_list = [url.replace('\n', '') for url in file]
     return urls_list
 
 
-def is_server_respond_with_200(url):    
+def is_server_respond_with_200(url):
     response = requests.get(url)
     if response.ok:
         return ('OK')
     else:
         return response.status_code
 
+
 def check_expiration_date(url):
-    whois_response = whois.whois(url)    
+    whois_response = whois.whois(url)
     first_exp_index = 0
     exp_date = whois_response['expiration_date'][first_exp_index]
     tz = exp_date.tzinfo
@@ -33,11 +34,9 @@ def check_expiration_date(url):
     max_exp_time = datetime.timedelta(days=30)
     days_before_exp = exp_date - now
     if days_before_exp > max_exp_time:
-        return 'more than 30'                      
+        return 'more than 30'
     else:
         return days_before_exp.days
-        
-        
 
 
 if __name__ == '__main__':
@@ -47,9 +46,5 @@ if __name__ == '__main__':
     for url in urls_list:
         url_status = is_server_respond_with_200(url)
         exp_status = check_expiration_date(url)
-        print(url, 'status: {}, days before expiration: {}'.format(url_status, exp_status))
-        
-
-
-
-    
+        print(url, 'status: {},
+              days before expiration: {}'.format(url_status, exp_status))
