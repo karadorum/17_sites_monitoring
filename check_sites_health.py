35d7af7e34cbf4441c8b2cdc16c05c45de1c088a
karadorum <contextstudio78@gmail.com>
2018-12-30 13:34:56 +0300

    Added all content

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,55 @@
+import requests
+import whois
+import argparse
+import datetime
+
+
+def console_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='Enter a path of file with urls')
+    arg = parser.parse_args()
+    return arg
+
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r', encoding='utf-8') as file:
+        urls_list =[url.replace('\n', '') for url in file]
+    return urls_list
 
-def is_server_respond_with_200(url):
-    pass
 
-def get_domain_expiration_date(domain_name):
-    pass
+def is_server_respond_with_200(url):    
+    response = requests.get(url)
+    if response.ok:
+        return ('OK')
+    else:
+        return response.status_code
+
+def check_expiration_date(url):
+    whois_response = whois.whois(url)    
+    first_exp_index = 0
+    exp_date = whois_response['expiration_date'][first_exp_index]
+    tz = exp_date.tzinfo
+    now = datetime.datetime.now(tz=tz)
+    max_exp_time = datetime.timedelta(days=30)
+    days_before_exp = exp_date - now
+    if days_before_exp > max_exp_time:
+        return 'more than 30'                      
+    else:
+        return days_before_exp.days
+        
+        
+
 
 if __name__ == '__main__':
-    pass
+    arguments = console_args()
+    urls_file_path = arguments.path
+    urls_list = load_urls4check(urls_file_path)
+    for url in urls_list:
+        url_status = is_server_respond_with_200(url)
+        exp_status = check_expiration_date(url)
+        print(url, 'status: {}, days before expiration: {}'.format(url_status, exp_status))
+        
+
+
+
+    
